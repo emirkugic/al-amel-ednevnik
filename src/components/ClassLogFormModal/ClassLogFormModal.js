e19a859d5f818fe1a01f5510d246ba6a51141b93
emirkugic <emir.kugic@hotmail.com>
2024-12-01 20:38:13 +0100

    Improved DesktopSidebarButton to handle many items at once

--- src/components/ClassLogFormModal/ClassLogFormModal.js
+++ src/components/ClassLogFormModal/ClassLogFormModal.js
@@ -79,6 +79,11 @@ const ClassLogFormModal = ({ onClose, departmentId }) => {
 						options={[
 							{ value: "1", label: "1st Period" },
 							{ value: "2", label: "2nd Period" },
+							{ value: "3", label: "3rd Period" },
+							{ value: "4", label: "4th Period" },
+							{ value: "5", label: "5th Period" },
+							{ value: "6", label: "6th Period" },
+							{ value: "7", label: "7th Period" },
 						]}
 					/>
 					<DropdownSelect
