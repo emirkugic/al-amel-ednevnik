24dba0133cb78ea09fc1375c91bcc38b3cf272d3
emirkugic <emir.kugic@hotmail.com>
2024-12-09 10:37:18 +0100

    BUG FIX: fixed import

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,7 +1,7 @@
 import React, { createContext, useState, useEffect } from "react";
 import authApi from "../api/authApi";
 import { useNavigate } from "react-router-dom";
-import jwtDecode from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 
 export const AuthContext = createContext();
 
