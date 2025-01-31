f60e2c9d69103b193af24961d2b2e7a59d97bb33
emirkugic <emir.kugic@hotmail.com>
2025-01-31 17:47:13 +0100

    Update API client base URL to use production endpoint

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-	baseURL: process.env.REACT_APP_API_URL || "http://localhost:5155/api",
-	// baseURL:
-	// 	process.env.REACT_APP_API_URL || "https://al-amel-api.onrender.com/api",
+	// baseURL: process.env.REACT_APP_API_URL || "http://localhost:5155/api",
+	baseURL:
+		process.env.REACT_APP_API_URL || "https://al-amel-api.onrender.com/api",
 });
 
 export default apiClient;
