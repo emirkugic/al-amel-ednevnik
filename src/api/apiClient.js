e0f9a6bf1b9cd352dbd476c5259dfa8b6d0e317a
emirkugic <emir.kugic@hotmail.com>
2025-02-01 15:40:35 +0100

    Update manifest icons and modify API client base URL configuration

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 
+const useDeployedAPI = true;
+
 const apiClient = axios.create({
-	baseURL: process.env.REACT_APP_API_URL || "http://localhost:5155/api",
-	// baseURL:
-	// 	process.env.REACT_APP_API_URL || "https://al-amel-api.onrender.com/api",
+	baseURL:
+		typeof useDeployedAPI !== "undefined"
+			? process.env.REACT_APP_API_URL || "https://al-amel-api.onrender.com/api"
+			: "http://localhost:5155/api",
 });
 
 export default apiClient;
