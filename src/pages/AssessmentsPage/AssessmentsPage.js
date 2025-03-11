50aa3f0717f9fc1965443a168d8274b32162f3d0
emirkugic <emir.kugic@hotmail.com>
2025-03-11 12:13:08 +0100

    Refactor: Removed Unused Components

--- src/pages/AssessmentsPage/AssessmentsPage.js
+++ src/pages/AssessmentsPage/AssessmentsPage.js
@@ -1,10 +1,7 @@
 import React from "react";
-import { useParams } from "react-router-dom";
 import AssessmentManagement from "./components/AssessmentManagement/AssessmentManagement";
 
 const AssessmentPage = () => {
-	const { subject } = useParams(); // Capture the subject id from the URL
-
 	return (
 		<div className="subject-assessment-management">
 			<AssessmentManagement />
